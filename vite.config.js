12f15d5ea31f63f00f6b666bd22fb6dd0fa06edb
KYO1717 <kyo1717171717@gmail.com>
2025-12-03 10:19:57 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Netlify에서 필요할 수 있는 기본 경로 설정 (선택 사항이지만 안전함)
+  base: '/', 
+})
